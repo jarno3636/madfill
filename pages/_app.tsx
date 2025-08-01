7fda73831c9fbf7413921548f1759d9732936281
jarno3636 <106784663+jarno3636@users.noreply.github.com>
2025-08-01 11:17:17 -0500

    Create _app.tsx

---
+++ pages/_app.tsx
@@ -0,0 +1,13 @@
+import '../styles/globals.css'
+import type { AppProps } from 'next/app'
+import { WagmiProvider } from '../lib/wagmi'
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <WagmiProvider>
+      <Component {...pageProps} />
+    </WagmiProvider>
+  )
+}
+
+export default MyApp
