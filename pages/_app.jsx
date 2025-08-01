11e038ff052c96e57aa73e313d56ef3197720f49
jarno3636 <106784663+jarno3636@users.noreply.github.com>
2025-08-01 15:42:17 -0500

    Update _app.jsx

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,6 +1,25 @@
 // pages/_app.jsx
-import '../styles/globals.css'
+import '@/styles/globals.css'
+import { WagmiConfig } from 'wagmi'
+import { createConfig, configureChains } from 'wagmi'
+import { publicProvider } from 'wagmi/providers/public'
+import { base } from 'wagmi/chains'
+import { farcasterMiniApp } from '@farcaster/miniapp-wagmi-connector'
+import { Button } from '@/components/ui/button'
+import { Card, CardContent, CardHeader } from '@/components/ui/card'
 
-export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const { chains, publicClient } = configureChains([base], [publicProvider()])
+
+const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors: [farcasterMiniApp()],
+  publicClient,
+})
+
+export default function App({ Component, pageProps }) {
+  return (
+    <WagmiConfig config={wagmiConfig}>
+      <Component {...pageProps} />
+    </WagmiConfig>
+  )
 }
