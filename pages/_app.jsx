7af9bf529c8025340df631eaaa988eb50fd1a6f8
jarno3636 <106784663+jarno3636@users.noreply.github.com>
2025-08-01 13:56:33 -0500

    Update _app.jsx

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,11 +1,6 @@
 // pages/_app.jsx
 import '../styles/globals.css'
-import { WagmiProvider } from '../lib/wagmi'
 
 export default function MyApp({ Component, pageProps }) {
-  return (
-    <WagmiProvider>
-      <Component {...pageProps} />
-    </WagmiProvider>
-  )
+  return <Component {...pageProps} />
 }
