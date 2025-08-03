4822d62fbcb6205b16bf554e2f69e329d964f5f6
jarno3636 <106784663+jarno3636@users.noreply.github.com>
2025-08-03 16:53:40 -0500

    Update og.js

--- pages/api/og.js
+++ pages/api/og.js
@@ -1,8 +1,8 @@
-// pages/api/og.js
 export default async function handler(req, res) {
   const { id } = req.query
+  const imageBuffer = await fetch('https://madfill.vercel.app/og/cover.png').then(r => r.arrayBuffer())
 
-  const imageBuffer = await generateOgImage(id) // Your logic here
   res.setHeader('Content-Type', 'image/png')
-  res.send(imageBuffer)
+  res.setHeader('Cache-Control', 'public, max-age=86400')
+  res.status(200).send(Buffer.from(imageBuffer))
 }
