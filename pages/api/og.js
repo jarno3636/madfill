105f1d2e113019208164560b65c440169df76826
jarno3636 <106784663+jarno3636@users.noreply.github.com>
2025-08-03 16:58:19 -0500

    Update og.jay

--- pages/api/og.js
+++ pages/api/og.js
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
   const { id } = req.query
-  const imageBuffer = await fetch('https://madfill.vercel.app/og/cover.png').then(r => r.arrayBuffer())
+  const imageBuffer = await fetch('https://madfill.vercel.app/og/cover.PNG').then(r => r.arrayBuffer())
 
   res.setHeader('Content-Type', 'image/png')
   res.setHeader('Cache-Control', 'public, max-age=86400')
