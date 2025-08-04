8007659c467d2d477b9178ac0628b78bc205c683
jarno3636 <106784663+jarno3636@users.noreply.github.com>
2025-08-04 10:26:55 -0500

    Update _app.jsx

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,6 +1,16 @@
 // pages/_app.jsx
 import '../styles/globals.css'
+import { useEffect } from 'react'
 
 export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    if (process.env.NODE_ENV === 'development') {
+      import('eruda').then((eruda) => {
+        eruda.init()
+        eruda.show()
+      })
+    }
+  }, [])
+
   return <Component {...pageProps} />
 }
