3c40cb5be4561ce36b75ce2115297f4a2c3ff226
jarno3636 <106784663+jarno3636@users.noreply.github.com>
2025-08-08 18:32:05 -0500

    Update _app.jsx

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,11 +1,14 @@
 // pages/_app.jsx
 import '../styles/globals.css'
 import ErrorBoundary from '@/components/ErrorBoundary'
+import { WagmiProvider } from '@/lib/wagmi'
 
 export default function App({ Component, pageProps }) {
   return (
     <ErrorBoundary>
-      <Component {...pageProps} />
+      <WagmiProvider>
+        <Component {...pageProps} />
+      </WagmiProvider>
     </ErrorBoundary>
   )
 }
