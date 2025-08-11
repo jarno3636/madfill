73e61246a678fe5cabd34b568e53b58f10f0ba56
jarno3636 <106784663+jarno3636@users.noreply.github.com>
2025-08-11 08:31:39 -0500

    Update _app.jsx

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -2,7 +2,36 @@
 'use client'
 
 import '@/styles/globals.css'
+import Head from 'next/head'
+import Layout from '@/components/Layout'
+import { DEFAULT_SEO } from '@/lib/seo'
+
+// If you have wallet/provider setup:
+// import { WagmiConfig } from 'wagmi'
+// import { config } from '@/lib/wagmi'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>{DEFAULT_SEO.title}</title>
+        <meta name="description" content={DEFAULT_SEO.description} />
+        <meta property="og:title" content={DEFAULT_SEO.title} />
+        <meta property="og:description" content={DEFAULT_SEO.description} />
+        <meta property="og:image" content={DEFAULT_SEO.image} />
+        <meta property="og:type" content="website" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content={DEFAULT_SEO.title} />
+        <meta name="twitter:description" content={DEFAULT_SEO.description} />
+        <meta name="twitter:image" content={DEFAULT_SEO.image} />
+      </Head>
+
+      {/* Uncomment if you use wagmi */}
+      {/* <WagmiConfig config={config}> */}
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      {/* </WagmiConfig> */}
+    </>
+  )
 }
