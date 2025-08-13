a83e258cbf8ef68643e5d3bee15a4c48eadac23e
jarno3636 <106784663+jarno3636@users.noreply.github.com>
2025-08-13 15:19:23 -0500

    Update utils.ts

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,12 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+// lib/utils.ts
 
-export function cn(...inputs: ClassValue[]) {
+import { type ClassValue, clsx } from 'clsx'
+import { twMerge } from 'tailwind-merge'
+
+/**
+ * Merge Tailwind classes with conditional inputs.
+ * Wraps `clsx` + `tailwind-merge`.
+ */
+export function cn(...inputs: ClassValue[]): string {
   return twMerge(clsx(inputs))
 }
