9ed8be30219d295c1fcd9ed8e7d35d177fafe92f
jarno3636 <106784663+jarno3636@users.noreply.github.com>
2025-08-13 09:29:50 -0500

    Update _document.jsx

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,23 +1,17 @@
 // pages/_document.jsx
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document'
 
-export default class MyDocument extends Document {
-  render() {
-    return (
-      <Html lang="en">
-        <Head>
-          {/* Keep this super minimal to avoid SSR merge issues */}
-          <link rel="icon" href="/favicon.ico" />
-          <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
-          <link rel="manifest" href="/site.webmanifest" />
-          {/* If you preload fonts, only do so with anonymous CORS */}
-          {/* <link rel="preload" href="/fonts/inter.woff2" as="font" type="font/woff2" crossOrigin="anonymous" /> */}
-        </Head>
-        <body className="antialiased">
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    )
-  }
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        {/* Minimal meta/icons only */}
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
 }
